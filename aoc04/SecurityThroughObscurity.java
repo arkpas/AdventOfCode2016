68914afe6c659dc8bb360d686b316080ab631f6c
Arkadiusz Paszenda <aro2457@gmail.com>
2019-06-10 18:38:24 +0200

    removed unused lines

--- aoc04/SecurityThroughObscurity.java
+++ aoc04/SecurityThroughObscurity.java
@@ -1,18 +1,15 @@
 package aoc04;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class SecurityThroughObscurity {
 	public static void main (String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/aoc04/rooms.txt"), "utf-8"));
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("src/aoc04/realrooms.txt"), "utf-8"));
+
 
 		try {
 			System.out.println("Sum of real rooms IDs: " + checkRooms(reader));
@@ -21,7 +18,6 @@ public class SecurityThroughObscurity {
 		catch (IOException a) {System.out.println("IOException!!!");}
 		
 		reader.close();
-		writer.close();
 	}
 	
 	public static long checkRooms (BufferedReader reader) throws IOException {
