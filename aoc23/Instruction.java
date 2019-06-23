ef60aded56421c02ad2d4dab20aa8e5370dd7fb3
Arkadiusz Paszenda <aro2457@gmail.com>
2019-06-23 14:31:34 +0200

    aoc12 update, aoc23 added

---
+++ aoc23/Instruction.java
@@ -0,0 +1,70 @@
+package aoc23;
+
+public class Instruction {
+	private String arg0 = "";
+	private char charArg1;
+	private char charArg2;
+	private int intArg1;
+	private int intArg2;
+	
+	public Instruction (String instruction) {
+		String[] split = instruction.split(" ");
+		arg0 = split[0];
+		charArg1 = split[1].charAt(0);
+		if (!Character.isLetter(charArg1))
+			intArg1 = Integer.parseInt(split[1]);
+		if (split.length > 2) {
+			charArg2 = split[2].charAt(0);
+			if (!Character.isLetter(charArg2))
+				intArg2 = Integer.parseInt(split[2]);
+		}
+	}
+	
+	public String getCommand () {
+		return arg0;
+	}
+	
+	public int getInt1 () {
+		return intArg1;
+	}
+	
+	public int getInt2 () {
+		return intArg2;
+	}
+	
+	public char getChar1 () {
+		return charArg1;
+	}
+	
+	public char getChar2 () {
+		return charArg2;
+	}
+	
+	public boolean isArg1Letter () {
+		return Character.isLetter(charArg1);
+	}
+	
+	public boolean isArg2Letter () {
+		return Character.isLetter(charArg2);
+	}
+	
+	public void setCommand (String command) {
+		arg0 = command;
+	}
+	
+	public String toString () {
+		StringBuilder str = new StringBuilder();
+		str.append(arg0 + " ");
+		if (isArg1Letter())
+			str.append(charArg1 + " ");
+		else
+			str.append(intArg1 + " ");
+		
+		if (isArg2Letter())
+			str.append(charArg2);
+		else
+			str.append(intArg2);
+		
+		return str.toString();
+	}
+}
--- aoc23/SafeCracking.java
+++ aoc23/SafeCracking.java
@@ -1,18 +1,20 @@
-package aoc12;
+package aoc23;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
-public class LeonardosMonorail {
+public class SafeCracking {
 	private static HashMap<Character, Integer> map = new HashMap<>();
 	
 	public static void main (String[] args){
 		double time = System.currentTimeMillis();
-		try (BufferedReader reader = new BufferedReader (new InputStreamReader (new FileInputStream("src/aoc12/instructions.txt"), "utf-8")))
+		try (BufferedReader reader = new BufferedReader (new InputStreamReader (new FileInputStream("src/aoc23/instructions.txt"), "utf-8")))
 			{
 		
 			reader.mark(1);
@@ -20,27 +22,28 @@ public class LeonardosMonorail {
 				reader.reset();
 			
 			String line = "";
-			ArrayList<Instruction> instructions = new ArrayList<>();
+			List<Instruction> instructions = new ArrayList<>();
+			List<Instruction> instructions2 = new ArrayList<>();
 			
 			while ((line=reader.readLine())!=null) {
-				Instruction ins = new Instruction(line);
-				instructions.add(ins);
+				instructions.add(new Instruction(line));
+				instructions2.add(new Instruction(line));
 			}
 			
 			System.out.println("Part 1: ");
-			map.put('a', 0);
+			map.put('a', 7);
 			map.put('b', 0);
 			map.put('c', 0);
 			map.put('d', 0);
 			execute(instructions);
 			
 			System.out.println("Part 2: ");
-			map.put('a', 0);
+			map.put('a', 12);
 			map.put('b', 0);
-			map.put('c', 1);
+			map.put('c', 0);
 			map.put('d', 0);
+			execute(instructions2);
 			
-			execute(instructions);
 			
 			time = System.currentTimeMillis() - time;
 			System.out.printf("Time: %.4f", time/1000);
@@ -49,12 +52,11 @@ public class LeonardosMonorail {
 
 	}
 	
-	public static void execute (ArrayList<Instruction> instructions) {
+	public static void execute (List<Instruction> instructions) {
 		int position = 0;
 		
 		while (position<instructions.size()) {
 			Instruction instruction = instructions.get(position);
-
 			switch (instruction.getCommand()) {
 				case "inc": {
 					map.put(instruction.getChar1(), map.get(instruction.getChar1())+1);
@@ -98,12 +100,55 @@ public class LeonardosMonorail {
 							position++;
 					break;
 				}
+				case "tgl": {
+					instructions = toggleInstructions(instructions);
+					position++;
+					break;
+				}
 
 			}
 
 		}
 		System.out.println("A: " + map.get('a'));
 			
+	}
+	
+	public static List<Instruction> toggleInstructions (List<Instruction> instructions) {
+		List<Instruction> toggled = new ArrayList<>();
+		toggled.addAll(Collections.unmodifiableList(instructions));
+		for (int i = 0; i < toggled.size(); i++) {
+			if (toggled.get(i).getCommand().equals("tgl")) {
+				int value = map.get(toggled.get(i).getChar1());
+				if (i + value >= 0 && i + value < toggled.size()) {
+					Instruction toToggle = toggled.get(i + value);
+					switch (toToggle.getCommand()) {
+						case "inc": {
+							toToggle.setCommand("dec");
+							break;
+						}
+						case "dec": {
+							toToggle.setCommand("inc");
+							break;
+						}
+						case "cpy": {
+							toToggle.setCommand("jnz");
+							break;
+						}
+						case "jnz": {
+							toToggle.setCommand("cpy");
+							break;
+						}
+						case "tgl": {
+							toToggle.setCommand("inc");
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		return toggled;
+		
 	}
 		
 		
